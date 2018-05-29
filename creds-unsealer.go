26fe45c8c6bd82496b11c3630953d4a46e8e2e0d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-05-29 10:02:28 +0200

    Use a small s for sirupsen

--- creds-unsealer.go
+++ creds-unsealer.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/camptocamp/creds-unsealer/config"
 	"github.com/camptocamp/creds-unsealer/providers"
